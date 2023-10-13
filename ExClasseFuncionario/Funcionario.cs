e73eaf2c0d0608e8e6d2b4fb9caad8810aa69108
Henrique Mendes <henriquemendes13@gmail.com>
2023-10-13 15:10:17 -0300

    Add project files.

---
+++ ExClasseFuncionario/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExClasseFuncionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalaraioBruto;
+        public double Imposto;
+        
+
+        public double SalarioLiquido()
+        {
+            return SalaraioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalaraioBruto = SalaraioBruto + (SalaraioBruto * ((porcentagem)*0.01));
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + "$" +SalarioLiquido().ToString("f2");
+        }
+
+        
+    }
+}
---
+++ ExClasseFuncionario/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExClasseFuncionario;
+
+Funcionario funcionario = new Funcionario();
+
+Console.Write("Nome: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Salário bruto: ");
+funcionario.SalaraioBruto = double.Parse(Console.ReadLine());
+
+Console.Write("Imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Funcionario: {funcionario}");
+
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+double porcentagem = double.Parse(Console.ReadLine());
+funcionario.AumentarSalario(porcentagem);
+
+Console.Write($"Dados atualizados: {funcionario}");
+
